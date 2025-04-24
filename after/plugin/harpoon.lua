4e4401af3ff4d6993390474c1e8bee229db579b3
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-04-24 10:11:21 +0200

    feat(harpoon): print file number

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,10 +4,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) vim.notify("In harpoon file 1", vim.log.levels.INFO, {title = "Harpoon", timeout = 5}) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) vim.notify("In harpoon file 1", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) vim.notify("In harpoon file 2", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) vim.notify("In harpoon file 3", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) vim.notify("In harpoon file 4", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
 
 
 
