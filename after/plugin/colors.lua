dd7c0b4d3e116ab3ba88ccf991c618bf2a543f57
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-09-03 01:17:43 +0200

    feat: ColorMyPencils with current omarchy theme unless color is provided or omarchy theme is not found.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,7 @@
 function ColorMyPencils(color)
 	-- color = color or "rose-pine"
 	-- color = color or "catppuccin"
-	color = color or "gruvbox"
+	color = color or theme[2].opts.colorscheme or "gruvbox"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
