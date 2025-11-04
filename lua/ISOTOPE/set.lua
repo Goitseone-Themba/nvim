c0b54fa11d4b80e02fa4995225516f184015c259
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-11-04 12:59:55 +0200

    style: disable netrw banner

--- lua/ISOTOPE/set.lua
+++ lua/ISOTOPE/set.lua
@@ -1,5 +1,12 @@
+-- enable line numbers and set them relative to the cursor's posision
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- configure netrw(built-in file explorer)
+vim.g.netrw_banner = 0
+-- vim.g.netrw_liststyle= 3
+
+vim.g.have_nerd_font = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -10,6 +17,7 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- disable swapfiles in exchange for undotree
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -29,3 +37,4 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
+
