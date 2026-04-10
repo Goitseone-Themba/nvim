73d30f89577680a9eb22a7d580d771f1a39b169e
Goitseone-Themba <goitseonerozthemba@gmail.com>
2026-04-10 19:14:20 +0200

    style: format

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,14 +1,26 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
+harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) vim.notify("In harpoon file 1", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) vim.notify("In harpoon file 2", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) vim.notify("In harpoon file 3", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) vim.notify("In harpoon file 4", vim.log.levels.INFO, {title = "Harpoon", timeout = 2}) end)
-
-
-
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) vim.notify("In harpoon file 1", vim.log.levels.INFO, {title = "Harpoon",timeout = 200}) -- also print("in harpoon file 1") also worked end, { desc = "Jump to first Harpoon file" })
+vim.keymap.set("n", "<C-h>",
+    function()
+        harpoon:list():select(1)
+        vim.notify("In harpoon file 1", vim.log.levels.INFO, { title = "Harpoon", timeout = 2 })
+    end)
+vim.keymap.set("n", "<C-t>",
+    function()
+        harpoon:list():select(2)
+        vim.notify("In harpoon file 2", vim.log.levels.INFO, { title = "Harpoon", timeout = 2 })
+    end)
+vim.keymap.set("n", "<C-n>",
+    function()
+        harpoon:list():select(3)
+        vim.notify("In harpoon file 3", vim.log.levels.INFO, { title = "Harpoon", timeout = 2 })
+    end)
+vim.keymap.set("n", "<C-s>",
+    function()
+        harpoon:list():select(4)
+        vim.notify("In harpoon file 4", vim.log.levels.INFO, { title = "Harpoon", timeout = 2 })
+    end)
