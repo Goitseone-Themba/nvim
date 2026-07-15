2423d1b76ad7780f57a84e788599f4189f1b4d84
Goitseone-Themba <goitseonerozthemba@gmail.com>
2026-07-15 16:09:02 +0200

    add error boundary will fix later

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,13 @@
+require("telescope").setup({})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', function()
+	local ok, err = pcall(builtin.git_files)
+	if not ok then
+		vim.notify("Not in a git repository", vim.log.levels.WARN)
+	end
+end, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("rg > ") });
 end)
